1c12a014a4aca7d1c44eae568fb2cc94af1f12cc
Cruz20025 <202014024@uns.edu.pe>
2025-03-05 11:10:51 -0500

    Update alumno.php

--- secciones/alumno.php
+++ secciones/alumno.php
@@ -1,1 +1,12 @@
+<?php
+
+$ciencia = [
+"nombres" => "Ruben Gonzalo",
+"apellidos" => "Cruz Acosta",
+"edad" => "24 años",
+"telefono" => "996701532",
+"correo" => "acostakaku420@gmail.com",
+
+];
+?>
 
