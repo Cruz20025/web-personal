3f1a9b4c750c6e2e3b9ae567a40ab4ddca67c6f8
Cruz20025 <202014024@uns.edu.pe>
2025-03-05 11:04:12 -0500

    Update and rename alumno to alumno.php

---
+++ alumno.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$ciencia = [
+"nombres" => "Ruben Gonzalo",
+"apellidos" => "Cruz Acosta",
+"edad" => "24 años",
+"telefono" => "996701532",
+"correo" => "acostakaku420@gmail.com",
+
+];
+?>
+
